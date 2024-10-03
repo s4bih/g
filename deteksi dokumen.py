f4682564f8ce373521ca8e312b6ed238f193d91f
msabi <msabihamin@gmail.com>
2024-10-03 21:47:49 +0800

    video rusak

--- deteksi dokumen.py
+++ deteksi dokumen.py
@@ -11,7 +11,6 @@ image_path = cv2.imread(image)
 
 point=[]
 dots=None
-imageu=None
 
 def draw_dots(event, x, y, flags, param):
     global point
@@ -26,6 +25,21 @@ def draw_dots(event, x, y, flags, param):
 
 def four_point_transform(image, pts):
     rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
 
 
 
